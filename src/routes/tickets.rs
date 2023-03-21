e209be58c601ef6a9e5f5a2767c5caf01f9296f6
ClementViste <clementviste36@gmail.com>
2023-03-21 14:46:03 +0100

    Update tickets handler to persist new tickets
    New tickets are now inserted into the database.

--- src/routes/tickets.rs
+++ src/routes/tickets.rs
@@ -1,5 +1,6 @@
 use actix_web::{web, HttpResponse};
 use serde::Deserialize;
+use sqlx::PgPool;
 
 /// Representation of a new ticket created with form data.
 #[derive(Deserialize)]
@@ -11,12 +12,38 @@ pub struct NewTicketFormData {
 /// Creates a new ticket.
 #[tracing::instrument(
     name = "Creating a new ticket",
-    skip(_form),
+    skip(pool, form),
     fields(
-        ticket_title = %_form.title,
-        ticket_description = %_form.description
+        ticket_title = %form.title,
+        ticket_description = %form.description
     )
 )]
-pub async fn create_ticket(_form: web::Form<NewTicketFormData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
+pub async fn create_ticket(
+    pool: web::Data<PgPool>,
+    form: web::Form<NewTicketFormData>,
+) -> HttpResponse {
+    match insert_ticket(&pool, &form).await {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(_) => HttpResponse::InternalServerError().finish(),
+    }
+}
+
+/// Inserts the new ticket details into the `tickets` table.
+#[tracing::instrument(
+    name = "Inserting the new ticket details into the tickets table",
+    skip(pool, form)
+)]
+pub async fn insert_ticket(pool: &PgPool, form: &NewTicketFormData) -> Result<(), sqlx::Error> {
+    sqlx::query!(
+        r#"
+        INSERT INTO tickets (title, description)
+        VALUES ($1, $2)
+        "#,
+        form.title,
+        form.description,
+    )
+    .execute(pool)
+    .await?;
+
+    Ok(())
 }
--- tests/api/tickets.rs
+++ tests/api/tickets.rs
@@ -12,6 +12,24 @@ async fn create_ticket_returns_a_200_when_valid_form_data() {
     assert_eq!(response.status().as_u16(), 200);
 }
 
+// Must persist a ticket,
+// when a `POST` request with valid form data is received at `/tickets/new`.
+#[tokio::test]
+async fn create_ticket_persists_the_new_ticket() {
+    let test_app = create_and_run_test_app().await;
+
+    let body = "title=Issue with ...&description=After doing ...";
+
+    test_app.post_tickets(body.into()).await;
+
+    let saved = sqlx::query!("SELECT title, description FROM tickets")
+        .fetch_one(&test_app.db_pool)
+        .await
+        .expect("Failed to fetch the saved ticket");
+    assert_eq!(saved.title, "Issue with ...");
+    assert_eq!(saved.description, "After doing ...");
+}
+
 // Must return a `400 Bad Request` response,
 // when a `POST` request with missing data is received at `/tickets/new`.
 #[tokio::test]
